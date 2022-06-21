a34904ea341a54b230af384bf28386ca4b22626e
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-06-21 23:04:47 +0200

    E8T3D2

--- src/shop/shop.service.ts
+++ src/shop/shop.service.ts
@@ -1,41 +1,29 @@
 import { forwardRef, Inject, Injectable } from "@nestjs/common";
-import { GetListOfProductsResponse } from "../interfaces/shop-item";
+import {GetListOfProductsResponse} from "../interfaces/shop-item";
 import { BasketService } from "../basket/basket.service";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {ShopItem} from "./shop-item.entity";
 
 @Injectable()
 export class ShopService {
 
     constructor(
       @Inject(forwardRef(()=> BasketService)) private basketService: BasketService,
+      @InjectRepository(ShopItem) private shopItemRespository: Repository<ShopItem>
     ) {
     }
 
-    getProducts(): GetListOfProductsResponse {
-        return [
-            {
-                name: "Czekolada",
-                description: "wspaniała czekolada gorzka",
-                priceNet: 2.30
-            },
-            {
-                name: "M&Ms",
-                description: "wspaniałe M&Ms",
-                priceNet: 5.30 - this.basketService.countPromo()
-            },
-            {
-                name: "7-Days",
-                description: "wspaniały 7-Days",
-                priceNet: 4.60 - this.basketService.countPromo()
-            }
-        ];
+    getProducts(): Promise<GetListOfProductsResponse> {
+        return await ShopItem.find();
     }
 
-    hasProduct(name: string): boolean {
-        return this.getProducts().some(item => item.name === name);
+    hasProduct(name: string): Promise<boolean> {
+        return (await this.getProducts()).some(item => item.name === name);
     }
 
-    getPriceOfProduct(name: string): number {
-        return this.getProducts().find(item=> item.name === name).priceNet;
+    getPriceOfProduct(name: string): Promise<number> {
+        return (await this.getProducts()).find(item=> item.name === name).priceNet;
     }
 
 }
