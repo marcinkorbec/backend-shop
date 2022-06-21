116388d0857b6a1b7b5a83057878dde5505bf497
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-06-21 23:05:06 +0200

    E8T3D2

---
+++ src/shop/shop-item.repository.ts
@@ -0,0 +1,17 @@
+// export const shopItemRepository = [
+//     {
+//         name: "Czekolada",
+//         description: "wspaniała czekolada gorzka",
+//         priceNet: 2.30
+//     },
+//     {
+//         name: "M&Ms",
+//         description: "wspaniałe M&Ms",
+//         priceNet: 5.30 - this.basketService.countPromo()
+//     },
+//     {
+//         name: "7-Days",
+//         description: "wspaniały 7-Days",
+//         priceNet: 4.60 - basketService.countPromo()
+//     }
+// ];
